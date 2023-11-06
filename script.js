24a39ff54e247f25bcde257f854b94e4d056760c
Mahdi <mahdimey17@gmail.com>
2023-11-06 23:13:49 +0330

    added function to increase tooltip counter

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const allTooltips = document.querySelectorAll('.skill_tooltip')
+
+allTooltips.forEach(tooltip => {
+    tooltip.innerText = ''
+
+    const updateCounter = () => {
+        const target = +tooltip.getAttribute('data-value')
+        const t = +tooltip.innerText
+
+        const increment = target / 100
+
+        if(t < target){
+            tooltip.innerText = `${Math.ceil(t + increment)}`
+            setTimeout( updateCounter, 1)
+        }
+        else{
+            tooltip.innerText = target + '%'
+        }
+    }
+
+    updateCounter()
+})
